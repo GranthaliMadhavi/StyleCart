20972110707a09b1cebbf56ef1785c58bffcb69b
GranthaliMadhavi <Granthalimadhavi248@gmail.com>
2026-08-31 20:04:41 +0530

    Started product page

--- src/main/java/com/stylecart/stylecart/repository/CartRepository.java
+++ src/main/java/com/stylecart/stylecart/repository/CartRepository.java
@@ -3,6 +3,7 @@ package com.stylecart.stylecart.repository;
 import com.stylecart.stylecart.entity.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 public interface CartRepository extends JpaRepository<Cart,Long>{
-    
+    List<Cart> findByUserId(Long cartId);
 }
